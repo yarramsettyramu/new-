3e522c5844672966d3d767f06937894e84024db4
yarramsettyramu <66875648+yarramsettyramu@users.noreply.github.com>
2020-09-12 11:29:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function myadd()
+{   
+    var a = document.getElementById("a");
+    var b = document.getElementById("b");
+    var c=parseInt(a.value)+parseInt(b.value);
+    document.write(c)
+}
+function mysub()
+{ 
+    var a=document.getElementById("a");
+    var b=document.getElementById("b");
+    var c=parseInt(a.value)-parseInt(b.value);
+    document.write(c);
+
+}
+function mymultiply()
+{
+    var a=document.getElementById("a");
+    var b=document.getElementById("b");
+    var c=parseInt(a.value)*parseInt(b.value);
+    document.write(c);
+}
+ 
